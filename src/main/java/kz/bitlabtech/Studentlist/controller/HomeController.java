8974fda24d5eb26439ed4bf259003502c0c3147d
Doни <daniyar.doskulov@narxoz.kz>
2023-05-16 17:19:58 +0600

    "Project2"

--- src/main/java/kz/bitlabtech/Studentlist/controller/HomeController.java
+++ src/main/java/kz/bitlabtech/Studentlist/controller/HomeController.java
@@ -1,7 +1,13 @@
 package kz.bitlabtech.Studentlist.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController{
+
+    @GetMapping(value = "/")
+    public String index(){
+        return "index";
+    }
 }
--- src/main/java/kz/bitlabtech/Studentlist/db/Student.java
+++ src/main/java/kz/bitlabtech/Studentlist/db/Student.java
@@ -1,4 +1,18 @@
 package kz.bitlabtech.Studentlist.db;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 public class Student {
+    private Long id;
+    private String name;
+    private String surname;
+    private int exam;
+    private String mark;
 }
