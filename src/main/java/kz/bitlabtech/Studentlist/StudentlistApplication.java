2fa14f9e4c50b253f701b8e0175cc6899c08d84f
Doни <daniyar.doskulov@narxoz.kz>
2023-05-16 15:56:53 +0600

    first commit

---
+++ src/main/java/kz/bitlabtech/Studentlist/StudentlistApplication.java
@@ -0,0 +1,13 @@
+package kz.bitlabtech.Studentlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentlistApplication.class, args);
+	}
+
+}
---
+++ src/main/java/kz/bitlabtech/Studentlist/controller/HomeController.java
@@ -0,0 +1,7 @@
+package kz.bitlabtech.Studentlist.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController{
+}
