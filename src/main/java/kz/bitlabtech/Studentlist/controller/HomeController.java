76aa160e54d33de43e063dc1f9174b5a46125673
Doни <daniyar.doskulov@narxoz.kz>
2023-05-16 18:34:15 +0600

    "Project"

--- src/main/java/kz/bitlabtech/Studentlist/controller/HomeController.java
+++ src/main/java/kz/bitlabtech/Studentlist/controller/HomeController.java
@@ -1,13 +1,30 @@
 package kz.bitlabtech.Studentlist.controller;
 
+import kz.bitlabtech.Studentlist.db.DBManager;
+import kz.bitlabtech.Studentlist.db.Student;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
 
 @Controller
 public class HomeController{
 
     @GetMapping(value = "/")
-    public String index(){
+    public String index(Model model){
+        ArrayList<Student> students = DBManager.getStudents();
+        model.addAttribute("student",students);
         return "index";
+    }
+    @PostMapping(value = "/add")
+    public String addStud(Student student) {
+        DBManager.addStud(student);
+        return "redirect:/";
+    }
+    @GetMapping(value = "/add")
+    public String addStud(){
+        return "addStud";
     }
 }
--- src/main/java/kz/bitlabtech/Studentlist/db/DBManager.java
+++ src/main/java/kz/bitlabtech/Studentlist/db/DBManager.java
@@ -1,4 +1,28 @@
 package kz.bitlabtech.Studentlist.db;
 
+import java.util.ArrayList;
+
 public class DBManager {
+
+    private static ArrayList<Student> students = new ArrayList<>();
+    private static Long id = 9L;
+
+    static {
+        students.add(new Student(1L,"Ilyas","Zhuanyshev",88,"B"));
+        students.add(new Student(2L,"Doni","Apwuberov",99,"A"));
+        students.add(new Student(3L,"Hitler","Adolf",87,"B"));
+        students.add(new Student(4L,"Joseph","Stalin",88,"B"));
+        students.add(new Student(5L,"Johnny","Depp",85,"B"));
+        students.add(new Student(6L,"Sponge","Bob",35,"F"));
+        students.add(new Student(7L,"Patrick","Star",1,"F"));
+        students.add(new Student(8L,"Kobe","Bryant",77,"B"));
+    }
+    public static ArrayList<Student> getStudents(){
+        return students;
+    }
+    public static void addStud(Student student){
+        student.setId(id);
+        id++;
+        students.add(student);
+    }
 }
