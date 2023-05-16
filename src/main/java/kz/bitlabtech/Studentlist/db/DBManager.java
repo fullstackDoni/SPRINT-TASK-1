405489ae0366b2604a123570c1381767b40ec8b4
Doни <daniyar.doskulov@narxoz.kz>
2023-05-16 20:04:07 +0600

    "Project"

--- src/main/java/kz/bitlabtech/Studentlist/db/DBManager.java
+++ src/main/java/kz/bitlabtech/Studentlist/db/DBManager.java
@@ -14,13 +14,24 @@ public class DBManager {
         students.add(new Student(4L,"Joseph","Stalin",88,"B"));
         students.add(new Student(5L,"Johnny","Depp",85,"B"));
         students.add(new Student(6L,"Sponge","Bob",35,"F"));
-        students.add(new Student(7L,"Patrick","Star",1,"F"));
+        students.add(new Student(7L,"Patrick","Star",15,"F"));
         students.add(new Student(8L,"Kobe","Bryant",77,"B"));
     }
     public static ArrayList<Student> getStudents(){
         return students;
     }
     public static void addStud(Student student){
+        if (student.getExam() >= 90) {
+            student.setMark("A");
+        } else if (student.getExam() >= 75) {
+            student.setMark("B");
+        } else if (student.getExam() >= 60) {
+            student.setMark("C");
+        } else if (student.getExam() >= 50) {
+            student.setMark("D");
+        } else {
+            student.setMark("F");
+        }
         student.setId(id);
         id++;
         students.add(student);
